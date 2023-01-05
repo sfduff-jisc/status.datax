dd2eb80c6e0f995cca39b69890d7f0d43fc59bb6
sfduff-jisc <sheridan.duff@jisc.ac.uk>
2023-01-05 17:28:13 +0000

    getting there

--- scrape.py
+++ scrape.py
@@ -18,9 +18,8 @@ print(p_element.text)
 print()
 
 
-services = []
-
 print('Services')
+services = []
 things = []
 things = p_element.find_elements(By.CSS_SELECTOR,"h2[class*='text-xl font-medium'")
 
@@ -30,21 +29,44 @@ for thing in things:
 print()
 
 print('Statuses')
+statuses = []
 states = []
 states = p_element.find_elements(By.CSS_SELECTOR,"p[class*='text-base'")
 
 for state in states:
     print(state.text)
+    statuses.append( state.text )
 
 print()
 
-print('other')
+print('times')
+times = []
 others = []
 others = p_element.find_elements(By.CSS_SELECTOR,"p[class*='text-sm'")
 
 for other in others:
     print(other.text)
+    times.append( other.text )
 
 
+print()
+print( services )
+print( statuses )
+print( times )
 
+status_page = {}
+
+
+for service in services:
+    status_page
+    for status in statuses:
+        for tim3 in times:
+            status_pa = {}
+            status_pa['service']=service
+            status_pa['status']=status
+            status_pa['time']=tim3
+            status_page.( status_pa )
+
+print()
+print( status_page )
 
