167b73d643741ccb9816c61b89e934331b15b42e
sfduff-jisc <sheridan.duff@jisc.ac.uk>
2023-01-06 10:37:47 +0000

    cleaner

--- scrape.py
+++ scrape.py
@@ -9,21 +9,18 @@ jspage.get('https://status.datax.jisc.ac.uk')
 time.sleep(2)
 
 # Gather item title in a list
-titles = []
 titles = jspage.find_elements(By.CSS_SELECTOR,"h2[class*='text-xl font-medium'")
 item_titles = []
 for title in titles:
     item_titles.append( title.text )
 
 # Gather item states in a list
-states = []
 states = jspage.find_elements(By.CSS_SELECTOR,"p[class*='text-base'")
 item_states = []
 for state in states:
     item_states.append( state.text )
 
 # Gather item update times in a list
-details = []
 details = jspage.find_elements(By.CSS_SELECTOR,"p[class*='text-sm'")
 item_details = []
 for other in details:
